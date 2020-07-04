00e270d3099786cbaa3a8bc7199d2febb227eb46
trey-causey <trey.causey@outlook.com>
2020-07-04 00:36:27 -0400

    JS and CSS showing up. Will encapsulate tomorrow

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="css/app.css" >
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+@include('partials.header')
+<div class="container" >@yield('content')</div>
+<p>There is a hidden message for you. Click to see it.</p>
+<button onclick="myFunction()">Click me!</button>
+<p id="demo"></p>
+<script type="text/javascript" src="js/group/shame.js"></script>
+</body>
+</html>
