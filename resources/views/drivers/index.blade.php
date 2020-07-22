4257bba06d3b1037623172d76f70994c82855e25
trey-causey <trey.causey@outlook.com>
2020-07-22 13:27:08 -0400

    fonts updated. Date formatting added to driver index page.t

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -6,13 +6,13 @@
     <p>Test sidebar</p>
 @endsection
 @section('content')
-    <div class="container bg-secondary">
+    <div class="driverContainer container bg-secondary">
         <div class="row">
             @foreach($drivers as $driver)
-                <div class="col-sm-12 col-md-6 col-lg-3" style="font-family: Calibri">
+                <div class="col-sm-12 col-md-6 col-lg-3" >
                     <img src="{{ $driver->portrait_url }}" id="driverPortrait" class="driverPortrait" height="175px" width="175px"><br>
-                    <h5>{{ $driver->first_name . " " . $driver->last_name }}</h5>
-                    <h5>{{ $driver->date_of_birth }}</h5>
+                    <h6>{{ $driver->first_name . " " . $driver->last_name }}</h6>
+                    <h6>{{ "D.O.B.: " . \Carbon\Carbon::parse($driver->date_of_birth)->toFormattedDateString() }}</h6>
                 </div>
             @endforeach
         </div>
