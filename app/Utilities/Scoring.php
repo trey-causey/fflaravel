4e290c434f90e8e1e632166d78f3c4f094b3981c
trey-causey <trey.causey@outlook.com>
2020-07-30 14:18:13 -0400

    accidentely worked on master. Commiting so I can branch out

--- app/Utilities/Scoring.php
+++ app/Utilities/Scoring.php
@@ -2,11 +2,27 @@
 
 namespace App\Utilities;
 
-use App\Models\Stats;
+use App\Teamowner;
+use App\Stats;
 
 class Scoring {
+const IT = 2;
 
-    public function calculatePoints() {
+private $driverPosition = [
+    1 => 25,
+    2 => 18,
+    3 => 15,
+    4 => 12,
+    5 => 10,
+    6 => 8,
+    7 => 6,
+    8 => 4,
+    9 => 2,
+    10 => 1
+];
+    public function calculatePoints()
+    {
+        $to = new Teamowner();
 
     }
 
